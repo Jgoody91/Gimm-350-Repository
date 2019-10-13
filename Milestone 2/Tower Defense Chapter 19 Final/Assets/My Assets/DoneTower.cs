f3baa7f7d2ebd01ff789a94f6bee24ee02180cb3
Jgoody91 <49137534+Jgoody91@users.noreply.github.com>
2019-10-12 18:34:26 -0600

    Uploaded At GimmLab

---
+++ Milestone 2/Tower Defense Chapter 19 Final/Assets/My Assets/DoneTower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoneTower : Tower {
+
+    public GameObject spikePrefab;
+
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+
+        // Shoot a stone
+        GameObject spike = (GameObject)Instantiate(spikePrefab, towerPieceToAim.position, Quaternion.identity);
+        spike.GetComponent<Spike>().enemyToFollow = targetEnemy;
+        spike.GetComponent<Spike>().damage = attackPower;
+    }
+}
---
+++ Milestone 2/Tower Defense Chapter 19 Final/Assets/My Assets/Spike.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Spike : FollowingProjectile
+    {
+        public float damage;
+
+        protected override void OnHitEnemy()
+        {
+            enemyToFollow.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+
